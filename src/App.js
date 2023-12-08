78a98ecf2bf0d44e6280b1703d8f91bfba44a9b4
DhruvGhatge <dhruvghatge11@gmail.com>
2023-12-08 22:16:57 +0530

    my project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
+
+import Menu from './components/Menu';
+
+import Main from './components/Main';
+import Header from './components/Header';
+
 
 function App() {
   return (
+ <>
+ <Nav/>
+ <Header/>
+ <Main/>
  
-    <div>
-      <h1>Meta Frontend Developer Capstone Starter Files</h1>
-    </div>
+ <Menu/>
+ 
+ </>
   );
-}
+};
 
 export default App;
